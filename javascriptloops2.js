07ae2367ed8634e3a3c058deb26b527ec0dfc327
Harish <kamandlaharish143@gmail.com>
2022-08-25 11:16:00 +0530

    javascriptloop2.js

---
+++ javascriptloops2.js
@@ -0,0 +1,11 @@
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+
+let len=cars.length
+// console.log(cars[12]);
+for (i=0;i<len;i++){
+    console.log(cars[i]);
+}
+
+
+for (i=0; i<10;i++)
+console.log(i);
